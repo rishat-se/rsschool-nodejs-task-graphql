b196dca26658cae9008e64cf1629e0cbdab72996
Rishat Agzamov <rishat.se@gmail.com>
2023-01-28 02:13:55 -0500

    feat: implement task 1

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -8,7 +8,9 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 ): Promise<void> => {
   fastify.get('/', async function (request, reply): Promise<
     MemberTypeEntity[]
-  > {});
+  > {
+    return await fastify.db.memberTypes.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +19,15 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const memberType = await fastify.db.memberTypes.findOne({
+        key: 'id',
+        equals: request.params.id,
+      });
+      if (!memberType)
+        throw fastify.httpErrors.notFound('member type not found');
+      return memberType;
+    }
   );
 
   fastify.patch(
@@ -28,7 +38,18 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      try {
+        return await fastify.db.memberTypes.change(
+          request.params.id,
+          request.body
+        );
+      } catch {
+        //author throws some error in change function if there is no memeber with required id
+        //rethrow error with 400 status code
+        throw fastify.httpErrors.badRequest('member type not found');
+      }
+    }
   );
 };
 
